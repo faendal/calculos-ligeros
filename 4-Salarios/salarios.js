d2f5e2c8723e2fcbb5a8a7785259bf94567fa59e
Faendal <juanjoborrero@yahoo.com>
2022-03-21 16:06:26 -0500

    Creación de los archivos necesarios para el taller 4

---
+++ 4-Salarios/salarios.js
@@ -0,0 +1,34 @@
+const colombia = [];
+
+function agregar(arreglo, nombre, salario)
+{
+    arreglo.push
+    (
+        {
+            name: nombre,
+            salary: salario
+        }
+    );
+}
+
+agregar(colombia, "Juanita", 500);
+agregar(colombia, "Natalia", 1500);
+agregar(colombia, "Luisa", 1800);
+agregar(colombia, "Laura", 1000);
+agregar(colombia, "Daniela", 2200);
+agregar(colombia, "Esperanza", 200);
+agregar(colombia, "Carla", 500);
+agregar(colombia, "Antonia", 1500);
+agregar(colombia, "Alicia", 1300);
+agregar(colombia, "Ana", 2400);
+agregar(colombia, "Julia", 3400);
+agregar(colombia, "Rosa", 400);
+agregar(colombia, "Angélica", 400);
+agregar(colombia, "Tatiana", 400);
+agregar(colombia, "Lorena", 1000);
+agregar(colombia, "Carolina", 1600);
+agregar(colombia, "Fernanda", 2600);
+agregar(colombia, "Nora", 1000);
+agregar(colombia, "Nicol", 10000);
+agregar(colombia, "Alejandra", 3000);
+agregar(colombia, "Paula", 2500);
